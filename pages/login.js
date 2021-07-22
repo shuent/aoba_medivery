ee31c60b561d4eaa807a79968ea13903c9d209af
shuent <shunshun.43@gmail.com>
2021-07-22 17:49:01 +0900

    change

--- pages/login.js
+++ pages/login.js
@@ -19,7 +19,6 @@ function SignInScreen() {
   return (
     <div>
       <h1>Login</h1>
-      <p>Please sign-in:</p>
       <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={fireAuth} />
     </div>
   );
