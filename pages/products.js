680eedb5aa196cb7b6c3a6e29c73fb66e2c740b4
Mattun1212 <kouta.dvlp@icloud.com>
2021-07-24 00:33:37 +0900

    Order complex

--- pages/products.js
+++ pages/products.js
@@ -3,7 +3,8 @@ import styles from "../styles/Home.module.css";
 import { useRouter } from 'next/router'
 import React,{ useEffect, useState } from 'react';
 import { getAllProducts } from "../repository/productRepository";
-import ProductCard from "../components/ProductCard.js";
+import { takeOrder } from "../usecases/takeOrder"
+import ProductCard from "../components/ProductCard";
 
 export default function Product() {
   const router = useRouter()
@@ -23,8 +24,50 @@ export default function Product() {
 
     const suggestProducts = products.filter(item => item.tag.includes(selectedTag))
 
+    // const [order, setOrder] = useState({id:'',quantity: 0})
+
+    const orderList = suggestProducts.map((suggestProduct)　=> {
+       return {quantity: 0,id: suggestProduct.id};
+      });
+
+    console.log(orderList)
+
+    const [count, setCount] = useState(0);
+
+    const checkOrder = (id) =>　{
+      const productIndex = orderList.findIndex(item => item.id === id)
+      console.log(orderList[productIndex])
+      return orderList[productIndex].quantity
+    }
+
+    const addOrder = (id) =>　{
+      const productIndex = orderList.findIndex(item => item.id === id)
+      console.log(orderList[productIndex])
+      
+
+
+
+      // const [change, setChange] = useState(orderList[productIndex])
+      // setChange({ quantity: change.quantity + 1 });
+    }
+
+    const minusOrder = (id) =>　{
+      const productIndex = orderList.findIndex(item => item.id === id)
+      console.log(orderList[productIndex])
+
+
+
+      // const [change, setChange] = useState(orderList[productIndex])
+      // setChange({ quantity: change.quantity - 1 });
+    }
+
+    const handleClick = ()=>{
+      const userId = "BZlUBvQz7tRRuT50yaIOS86AiPl2" // should be currentUser id
+      takeOrder(userId, orderList)
+    }
 
   return (
+    <>
     <div>
       <Head>
         <title>レコメンド商品一覧</title>
@@ -43,22 +86,25 @@ export default function Product() {
       <main className="container">
         <h1 className="title">レコメンド商品一覧</h1>
         {suggestProducts.map((suggestProduct) => (
+          // <div>
+          //  <ProductCard product={suggestProduct} />
+          // </div>
           <div>
-           <ProductCard product={suggestProduct} />
-          </div>
-        ))}
+            <p>{suggestProduct.name}</p>
+            <p>{suggestProduct.explanation}</p>    
+            <p>{suggestProduct.price * checkOrder(suggestProduct.id)}円</p>
+            <p><button onClick={() => addOrder(suggestProduct.id)}>+</button>{checkOrder(suggestProduct.id)}<button onClick={() => {minusOrder(suggestProduct.id)}}>-</button></p>
+        </div>
+        ))
+        }
         <div>
-          <button
-            className="button"
-            type="submit"
-          >
-          注文
-          </button>
+          <button onClick={handleClick}>注文</button>
         </div>
 
       </main>
 
       <footer className={styles.footer}>Powered by Aoba</footer>
     </div>
+    </>
   );
 } 
