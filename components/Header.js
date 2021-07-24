fd514f274622dc7353da013f3e23fe81e8a3f756
Yoshi-Aki1015 <s2010845@s.tsukuba.ac.jp>
2021-07-24 09:14:11 +0900

    header途中

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import { fireAuth } from "../lib/firebase";
 import { AuthContext } from "../hooks/useAuth";
 import { useContext } from "react";
 import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 function Header() {
@@ -15,21 +16,64 @@ function Header() {
   };
 
   return (
-    <>
-      {currentUser ? (
-        <div>
-          <div>
-            
-            <Link href={`/users/${currentUser.uid}`}>
-              <a> My Page: {currentUser.displayName}</a>
+
+    <div>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" >
+            <Link href={`/`}>
+              <a className={`has-text-weight-bold ${styles.head_title}`}>Medivery</a>
             </Link>
-          </div>
-          <button onClick={handleLogout}>Logout</button>
+          </a>
+
+          <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
         </div>
-      ) : (
-        <div> Please login</div>
-      )}
-    </>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <Link href={`/form`}>
+              <a class="navbar-item">診断フォーム</a>
+            </Link>
+            <a class="navbar-item">Documentation</a>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-info">
+
+
+
+                {currentUser ? (
+                  <div>
+                    <div>
+                      <Link href={`/users/${currentUser.uid}`}>
+                        <a class="button is-light"> マイページへ: {currentUser.displayName}</a>
+                      </Link>
+                    </div>
+                    <a class="button is-light" onClick={handleLogout}>ログアウト</a>
+                  </div>
+                ) : (
+                  <div>
+                    <Link href={`/login`}>
+                      <a> ログインページへ</a>
+                    </Link>
+                  </div>
+
+                )}
+
+        
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 }
 
